92a6251119bb0ff491b238294745a01e2f686d1e
viniciuskociolek58797 <viniciuskociolek58797@gmail.com>
2025-02-09 18:50:43 +0000

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,153 @@
+import 'package:flutter/material.dart';
+
+import 'controles/controle_planeta.dart';
+import 'modelos/planeta.dart';
+import 'telas/tela_planeta.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Planetas',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 87, 218, 126)),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Aplicativo CRUD - Planetas'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final ControlePlaneta _controlePlaneta = ControlePlaneta();
+  List<Planeta> _planetas = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _lerPlanetas();
+  }
+
+  Future<void> _lerPlanetas() async {
+    final resultado = await _controlePlaneta.lerPlanetas();
+    setState(() {
+      _planetas = resultado;
+    });
+  }
+
+  void _incluirPlaneta(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => TelaPlaneta(
+          isIncluir: true,
+          planeta: Planeta.vazio(),
+          Finalizado: () {
+            _lerPlanetas();
+          },
+        ),
+      ),
+    );
+  }
+
+  void _alterarPlaneta(BuildContext context, Planeta planeta) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => TelaPlaneta(
+          isIncluir: false,
+          planeta: planeta,
+          Finalizado: () {
+            _lerPlanetas();
+          },
+        ),
+      ),
+    );
+  }
+
+  void _excluirPlaneta(BuildContext context, int id) async {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Confirmação'),
+          content: const Text('Deseja realmente excluir este planeta?'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(); 
+              },
+              child: const Text('Cancelar'),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                await _controlePlaneta.excluirPlaneta(id);
+                _lerPlanetas();
+                Navigator.of(context).pop(); 
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.red,
+              ),
+              child: const Text('Excluir', style: TextStyle(color: Colors.white)),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: ListView.builder(
+        itemCount: _planetas.length,
+        itemBuilder: (context, index) {
+          final planeta = _planetas[index];
+          return ListTile(
+            title: Text(planeta.nome),
+            subtitle: Text(planeta.apelido!),
+            trailing: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                IconButton(
+                  icon: const Icon(Icons.edit),
+                  onPressed: () => _alterarPlaneta(context, planeta),
+                ),
+                IconButton(
+                  icon: const Icon(Icons.delete),
+                  onPressed: () => _excluirPlaneta(context, planeta.id!),
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _incluirPlaneta(context);
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
